75afaf7aba2e26caa907e75785c8e90349ef4a7f
jcltd303-hub <jcltd303@gmail.com>
2026-09-24 08:17:52 -0600

    feat(render): use Spine tetra rig for small fish

--- src/engine/systems/Fish.ts
+++ src/engine/systems/Fish.ts
@@ -5,6 +5,7 @@ import { EntityBounds, SpatialHashGrid } from './SpatialHashGrid';
 import { IRenderRig } from './RenderRig';
 import { FishRenderRig } from './FishRenderRig';
 import { BossRenderRig } from './BossRenderRig';
+import { SpineTetraRenderRig } from './SpineTetraRenderRig';
 
 export class Fish implements Boid {
   public id: string;
@@ -46,8 +47,12 @@ export class Fish implements Boid {
     if (isBoss) {
       this.renderRig = new BossRenderRig(this.width, this.height, theme);
     } else {
-      const animRig = SpriteSheetManager.getInstance().createFishAnimationRig(type, theme);
-      this.renderRig = new FishRenderRig(animRig);
+      if (isSmall && SpineTetraRenderRig.isReady()) {
+        this.renderRig = new SpineTetraRenderRig(theme);
+      } else {
+        const animRig = SpriteSheetManager.getInstance().createFishAnimationRig(type, theme);
+        this.renderRig = new FishRenderRig(animRig);
+      }
       if (this.facing === 'left') {
         this.renderRig.playState('swim_left');
       } else {
